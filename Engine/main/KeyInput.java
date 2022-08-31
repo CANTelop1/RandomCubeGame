2843d751021c4fb715d0a6fb5997cc94156a1d04
Lachlan Williams <lwil0039@student.monash.edu>
2022-08-31 14:55:58 +1000

    KeyInput jumps player with arrows simplified

--- Engine/main/KeyInput.java
+++ Engine/main/KeyInput.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class KeyInput extends KeyAdapter {
     private Handler handler;
-    private ArrayList<Integer> currentPressed = new ArrayList<Integer>();
+    private int currentPressed;
 
     public KeyInput(Handler handler){
         this.handler = handler;
@@ -18,8 +18,7 @@ public class KeyInput extends KeyAdapter {
     @Override
     public void keyPressed(KeyEvent e){
         int key = e.getKeyCode();
-        currentPressed.clear();
-        currentPressed.add(key);
+        currentPressed = key;
 
         for(int i = 0; i < handler.objects.size(); i++){
             GameObject tempObject = handler.objects.get(i);
@@ -43,30 +42,17 @@ public class KeyInput extends KeyAdapter {
             GameObject tempObject = handler.objects.get(i);
 
             if(tempObject.getId() == ID.player){
+                //moves player object
                 if(key == KeyEvent.VK_W) tempObject.setVelY(0);
                 if(key == KeyEvent.VK_S) tempObject.setVelY(0);
                 if(key == KeyEvent.VK_A) tempObject.setVelX(0);
                 if(key == KeyEvent.VK_D) tempObject.setVelX(0);
-                for(int j = 0; j < currentPressed.size();j++){
-                    if(currentPressed.get(j) == key && key == KeyEvent.VK_UP){
-                        //currentPressed.remove(j);
-                        tempObject.setY(tempObject.getY()-50);
-                    }
-                    if(currentPressed.get(j) == key && key == KeyEvent.VK_DOWN){
-                        //currentPressed.remove(j);
-                        tempObject.setY(tempObject.getY()+50);
-                    }
-                    if(currentPressed.get(j) == key && key == KeyEvent.VK_RIGHT){
-                        //currentPressed.remove(j);
-                        tempObject.setX(tempObject.getX()+50);
-                    }
-                    if(currentPressed.get(j) == key && key == KeyEvent.VK_LEFT){
-                        //currentPressed.remove(j);
-                        tempObject.setX(tempObject.getX()-50);
-                    }
+
+                if(currentPressed == key && key == KeyEvent.VK_UP)tempObject.setY(tempObject.getY()-50);
+                if(currentPressed == key && key == KeyEvent.VK_DOWN)tempObject.setY(tempObject.getY()+50);
+                if(currentPressed == key && key == KeyEvent.VK_RIGHT)tempObject.setX(tempObject.getX()+50);
+                if(currentPressed == key && key == KeyEvent.VK_LEFT)tempObject.setX(tempObject.getX()-50);
                 }
             }
         }
-
     }
-}
