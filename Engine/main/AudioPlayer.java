160fa17b5d0faba77f6213f9e891f89f2b4941ab
Lachlan Williams <lwil0039@student.monash.edu>
2022-10-05 14:52:10 +1100

    AudioPlayer class created

---
+++ Engine/main/AudioPlayer.java
@@ -0,0 +1,11 @@
+package Engine.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//import org.newdawn.slick.Music;
+//import org.newdawn.slick.SlickException;
+//import org.newdawn.slick.Sound;
+
+public class AudioPlayer {
+}
